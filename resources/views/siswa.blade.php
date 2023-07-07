86d734f373a49dbc7576a75229fb90e414d1f131
anggagumilang212 <anggagumilang212@gmail.com>
2023-07-07 13:39:19 +0700

    membuat model guru dan menambahkan button pada siswa

--- resources/views/siswa.blade.php
+++ resources/views/siswa.blade.php
@@ -1,12 +1,15 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Siswa</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  </head>
-  <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
 
     <div class="container">
 
@@ -16,14 +19,14 @@
 
         <table class="table table-striped table-hover mt-4">
             <thead>
-              <tr>
-                <th scope="col">NO</th>
-                <th scope="col">Nama</th>
-                <th scope="col">Kelas</th>
-                <th scope="col">Jenis Kelamin</th>
-                <th scope="col">Alamat</th>
-                <th scope="col">Aksi</th>
-              </tr>
+                <tr>
+                    <th scope="col">NO</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Kelas</th>
+                    <th scope="col">Jenis Kelamin</th>
+                    <th scope="col">Alamat</th>
+                    <th scope="col">Aksi</th>
+                </tr>
             </thead>
             <tbody>
                 @foreach ($siswa as $data)
@@ -34,13 +37,13 @@
                         <td>{{ $data->jk }}</td>-
                         <td>{{ $data->alamat }}</td>
                         <td>
-                            <a href=# data-bs-target="#  {{ $data->id }}"
-                                data-bs-toggle="modal" class="btn btn-warning">Edit</a>
+                            <a href=# data-bs-target="#  {{ $data->id }}" data-bs-toggle="modal"
+                                class="btn btn-warning">Edit</a>
                         </td>
                     </tr>
                 @endforeach
             </tbody>
-      </table>
+        </table>
     </div>
 
 
@@ -50,106 +53,112 @@
     {{-- Modal Tambah Data --}}
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Siswa</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="tambah-siswa" method="POST">
-                    @csrf
-                    <div class="mb-3">
-                      <label for="nama" class="form-label">Nama</label>
-                      <input type="text" class="form-control" name="nama" id="nama" aria-describedby="">
-                    </div>
-                    <div class="mb-3">
-                        <label for="Kelas" class="form-label">Kelas</label>
-                        <select class="form-select" name="id_kelas" aria-label="Default select example">
-                            <option selected>Pilih Kelas</option>
-                            <option value="10">Kelas 10</option>
-                            <option value="11">Kelas 11</option>
-                            <option value="12">Kelas 12</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="jk" class="form-label">Jenis Kelamin</label>
-                        <select class="form-select" name="jk" aria-label="Default select example">
-                            <option selected>Pilih Jenis Kelamin</option>
-                            <option value="Laki-Laki">Laki-Laki</option>
-                            <option value="Perempuan">Perempuan</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="almaat" class="form-label">Alamat</label>
-                        <textarea class="form-control" name="alamat"  id=""></textarea>
-                    </div>
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Siswa</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="tambah-siswa" method="POST">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="nama" class="form-label">Nama</label>
+                            <input type="text" class="form-control" name="nama" id="nama"
+                                aria-describedby="">
+                        </div>
+                        <div class="mb-3">
+                            <label for="Kelas" class="form-label">Kelas</label>
+                            <select class="form-select" name="id_kelas" aria-label="Default select example">
+                                <option selected>Pilih Kelas</option>
+                                <option value="10">Kelas 10</option>
+                                <option value="11">Kelas 11</option>
+                                <option value="12">Kelas 12</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="jk" class="form-label">Jenis Kelamin</label>
+                            <select class="form-select" name="jk" aria-label="Default select example">
+                                <option selected>Pilih Jenis Kelamin</option>
+                                <option value="Laki-Laki">Laki-Laki</option>
+                                <option value="Perempuan">Perempuan</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="almaat" class="form-label">Alamat</label>
+                            <textarea class="form-control" name="alamat" id=""></textarea>
+                        </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
                     <button type="submit" class="btn btn-primary">Simpan</button>
                 </div>
-            </form>
-          </div>
+                </form>
+            </div>
         </div>
     </div>
-      {{-- End Modal Tambah Data --}}
+    {{-- End Modal Tambah Data --}}
 
     {{-- Modal Edit Data --}}
-    @foreach ( $siswa as $item )
-    <div class="modal fade" id="formModalEdit{{ $item->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Siswa</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    @foreach ($siswa as $item)
+        <div class="modal fade" id="formModalEdit{{ $item->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Siswa</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="edit-siswa" method="POST">
+                            @csrf
+                            <button>Hello</button>
+                            <input type="hidden" class="form-control" id="id" name="id"
+                                value="{{ $item->id }}">
+
+                            <div class="mb-3">
+                                <label for="nama" class="form-label">Nama</label>
+                                <input type="text" class="form-control" name="nama" id="nama"
+                                    aria-describedby="">
+                            </div>
+                            <div class="mb-3">
+                                <label for="Kelas" class="form-label">Kelas</label>
+                                <select class="form-select" name="id_kelas" aria-label="Default select example">
+                                    <option selected>Pilih Kelas</option>
+                                    <option value="10">Kelas 10</option>
+                                    <option value="11">Kelas 11</option>
+                                    <option value="12">Kelas 12</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="jk" class="form-label">Jenis Kelamin</label>
+                                <select class="form-select" name="jk" aria-label="Default select example">
+                                    <option selected>Pilih Jenis Kelamin</option>
+                                    <option value="Laki-Laki">Laki-Laki</option>
+                                    <option value="Perempuan">Perempuan</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="almaat" class="form-label">Alamat</label>
+                                <textarea class="form-control" name="alamat" id=""></textarea>
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                    </form>
+                </div>
             </div>
-            <div class="modal-body">
-                <form action="edit-siswa" method="POST">
-                    @csrf
-
-                    <input type="hidden" class="form-control" id="id" name="id"
-                    value="{{ $item->id }}">
-
-                    <div class="mb-3">
-                      <label for="nama" class="form-label">Nama</label>
-                      <input type="text" class="form-control" name="nama" id="nama" aria-describedby="">
-                    </div>
-                    <div class="mb-3">
-                        <label for="Kelas" class="form-label">Kelas</label>
-                        <select class="form-select" name="id_kelas" aria-label="Default select example">
-                            <option selected>Pilih Kelas</option>
-                            <option value="10">Kelas 10</option>
-                            <option value="11">Kelas 11</option>
-                            <option value="12">Kelas 12</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="jk" class="form-label">Jenis Kelamin</label>
-                        <select class="form-select" name="jk" aria-label="Default select example">
-                            <option selected>Pilih Jenis Kelamin</option>
-                            <option value="Laki-Laki">Laki-Laki</option>
-                            <option value="Perempuan">Perempuan</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="almaat" class="form-label">Alamat</label>
-                        <textarea class="form-control" name="alamat"  id=""></textarea>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                </div>
-            </form>
-          </div>
         </div>
-    </div>
     @endforeach
-      {{-- End Modal Edit Data --}}
+    {{-- End Modal Edit Data --}}
 
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
